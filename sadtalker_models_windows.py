49010155fe0c45d9570cf9208b535c3f6557560e
Thirupathi <smartbotindia@gmail.com>
2025-06-22 02:50:45 +0530

    Updated model downloader script for Windows

--- sadtalker_models_windows.py
+++ sadtalker_models_windows.py
@@ -1,13 +1,13 @@
 import os
 import urllib.request
 
-# Define model files and correct URLs
+# Define model files and their URLs
 MODEL_URLS = {
     "checkpoints/mapping_00109-model.pth.tar": "https://github.com/OpenTalker/SadTalker/releases/download/v0.0.2-rc/mapping_00109-model.pth.tar",
     "checkpoints/mapping_00229-model.pth.tar": "https://github.com/OpenTalker/SadTalker/releases/download/v0.0.2-rc/mapping_00229-model.pth.tar",
     "checkpoints/SadTalker_V0.0.2_256.safetensors": "https://github.com/OpenTalker/SadTalker/releases/download/v0.0.2-rc/SadTalker_V0.0.2_256.safetensors",
-    # Fixed GFPGAN v1.3 model URL
-    "checkpoints/gfpgan/GFPGANv1.3.pth": "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth"
+    # Optional GFPGAN (used for face enhancement)
+    "checkpoints/gfpgan/GFPGANv1.3.pth": "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.8/GFPGANv1.3.pth"
 }
 
 def ensure_directories():
